9dcd7adfef83a2f3e4ec4aa27e1ea53d24d04e7a
Lukas Zapolskas <lukas.zapolskas@gmail.com>
2018-12-10 20:02:16 +0400

    Tweaked frontend and recording script

--- background.js
+++ background.js
@@ -21,14 +21,37 @@ And TOTALLY isn't designed to make you more wary of the things you install onto 
 
 
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.local.set({"init_time": new Date().getTime()});
-  chrome.storage.local.set({"percount": 0});
+  chrome.storage.local.set({ "init_time": new Date().getTime() });
+  chrome.storage.local.set({ "percount": 0 });
 });
 
 chrome.runtime.onStartup.addListener(function () {
   // TODO Look for local storage, and load from there
   let check_and_show_page = function (result) {
-    return 5;
+    let time = parseInt(result);
+    if (time != NaN) {
+      // Get number of seconds elapsed. Currently setting it for two hours
+      if (Math.round(time / 1000) <= 7200) {
+        chrome.tabs.create({
+          active: true,
+          url: chrome.runtime.getURL("frontend.html")
+        }, function (tab) {
+          if (tab.url == "frontend.html") {
+            const facebook = document.getElementById("facebook");
+            const instagram = document.getElementById("instagram");
+            const snapchat = document.getElementById("snapchat");
+
+            // Set display: none if not in browsing history
+            const personalities = document.getElementById("personality");
+            const b5_list = document.createElement("ul");
+            personalities.after(b5_list);
+
+            // TODO Add list items here. Something with appendChild, not sure
+
+          }
+        })
+      }
+    }
   } // placeholder. Put actual code into this function l8r
 
   chrome.storage.local.get(['init_time'], check_and_show_page(result));
@@ -63,46 +86,46 @@ chrome.runtime.onSuspend.addListener(function () {
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, Tab) {
   if (changeInfo.status === "complete" && changeInfo.url) {
     chrome.storage.local.get(['percount'], function (result) {
-        if (result < 3) {
-          const paragraphs = Array.prototype.slice
-            .call(document.getElementsByTagName("p"))
-            .map((data) => data.innerHTML).join(" ");
-          const request = new XMLHttpRequest();
-           request.open("POST", "https://gateway-syd.watsonplatform.net/personality-insights/api",
-            true, "sb5455+watson@nyu.edu", "CoolCoolEvan42069");
+      if (result < 3) {
+        const paragraphs = Array.prototype.slice
+          .call(document.getElementsByTagName("p"))
+          .map((data) => data.innerHTML).join(" ");
+        const request = new XMLHttpRequest();
+        request.open("POST", "https://gateway-syd.watsonplatform.net/personality-insights/api",
+          true, "sb5455+watson@nyu.edu", "CoolCoolEvan42069");
 
-          request.setRequestHeader("X-Watson-Learning-Opt-Out", true);
-          request.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+        request.setRequestHeader("X-Watson-Learning-Opt-Out", true);
+        request.setRequestHeader("Content-Type", "application/json;charset=utf-8");
 
-          request.onreadystatechanged = function () {
-            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-              let p_traits = JSON.parse(http.responseText).personality;
-              p_traits.sort(objNameCompare(a, b));
+        request.onreadystatechanged = function () {
+          if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            let p_traits = JSON.parse(http.responseText).personality;
+            p_traits.sort(objNameCompare(a, b));
 
-              let stored_b5_traits = [];
-              chrome.storage.local.get(["b5_traits"], function (result) {
-                stored_b5_traits = JSON.parse(result);
-              });
+            let stored_b5_traits = [];
+            chrome.storage.local.get(["b5_traits"], function (result) {
+              stored_b5_traits = JSON.parse(result);
+            });
 
 
-              if (!stored_b5_traits.length) {
-                stored_b5_traits = p_traits;
-              } else {
-                stored_b5_traits.sort(objNameCompare(a, b));
-                for (let i = 0; i < p_traits.length; i++) {
-                  if (stored_b5_traits[i].name === p_traits[i].name) {
-                    stored_b5_traits[i].percentile += p_traits[i].percentile;
-                  }
+            if (!stored_b5_traits.length) {
+              stored_b5_traits = p_traits;
+            } else {
+              stored_b5_traits.sort(objNameCompare(a, b));
+              for (let i = 0; i < p_traits.length; i++) {
+                if (stored_b5_traits[i].name === p_traits[i].name) {
+                  stored_b5_traits[i].percentile += p_traits[i].percentile;
                 }
               }
-
-              chrome.storage.local.set({"b5_traits": stored_b5_traits});
             }
-          };
-          request.send(paragraphs);
-          chrome.storage.local.set({"percount": ++result});
-        }
+
+            chrome.storage.local.set({ "b5_traits": stored_b5_traits });
+          }
+        };
+        request.send(paragraphs);
+        chrome.storage.local.set({ "percount": ++result });
       }
+    }
     )
   }
 });
